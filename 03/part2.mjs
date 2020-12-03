c78d89e3ca0f534db8c5e42d0ea2e672f4adf4d1
Svante Bengtson <svante.b37@gmail.com>
2020-12-03 10:25:34 +0100

    add day 3

---
+++ 03/part2.mjs
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs'
+
+/** @type {Array<Array<string>>} array[y][x] */
+const input = readFileSync('./input.txt', { encoding: 'utf-8' })
+  .split('\n')
+  .filter(row => row.length)
+  .map(row => row.split(''))
+const width = input[0].length
+
+const slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+]
+
+let multiTrees = 1
+
+for (const [vx, vy] of slopes) {
+  let x = 0, y = 0
+  let encounters = 0
+
+  do {
+    if (input[y][x % width] === '#') encounters++
+
+    x += vx
+    y += vy
+  } while (y < input.length)
+
+  multiTrees *= encounters
+}
+
+console.log(multiTrees)
