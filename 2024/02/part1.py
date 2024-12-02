0837452771ccd87dc4ec3e0efbc09758b8c9866a
Svante Bengtson <svante@swantzter.se>
2024-12-02 20:35:15 +0100

    add day 2

---
+++ 2024/02/part1.py
@@ -0,0 +1,24 @@
+import sys
+import pathlib
+
+DEFAULT_FILE = pathlib.Path(__file__).parent.resolve().joinpath('data.txt')
+
+def parse () -> list[list[int]]:
+  return [
+    list(map(int, line.split())) for line in open(sys.argv[1] if len(sys.argv) > 1 else DEFAULT_FILE, 'r')
+  ]
+
+def main ():
+  reports = parse()
+  num_safe: int = 0
+
+  for report in reports:
+    inc_dec = all(i < j for i, j in zip(report, report[1:])) or all(i > j for i, j in zip(report, report[1:]))
+    safe_inc = max(abs(a - b) for a, b in zip(report, report[1:])) <= 3
+
+    num_safe += 1 if inc_dec and safe_inc else 0
+
+  print(num_safe)
+
+if __name__ == "__main__":
+  main()
---
+++ 2024/02/part2.py
@@ -0,0 +1,26 @@
+import sys
+import pathlib
+import itertools
+
+DEFAULT_FILE = pathlib.Path(__file__).parent.resolve().joinpath('data.txt')
+
+def parse () -> list[list[int]]:
+  return [
+    list(map(int, line.split())) for line in open(sys.argv[1] if len(sys.argv) > 1 else DEFAULT_FILE, 'r')
+  ]
+
+def safe (report: list[int]) -> bool:
+  inc_dec =  all(i < j for i, j in zip(report, report[1:])) or all(i > j for i, j in zip(report, report[1:]))
+  safe_inc = max(abs(a - b) for a, b in zip(report, report[1:])) <= 3
+
+  return True if inc_dec and safe_inc else False
+
+def main ():
+  reports = parse()
+  num_safe: int = sum(any(safe(r) for r in itertools.combinations(report, len(report) - 1)) for report in reports)
+
+
+  print(num_safe)
+
+if __name__ == "__main__":
+  main()
