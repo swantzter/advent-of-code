3e13e05e80f42ba3bf05f5fe61b4674c76e47ff7
Svante Bengtson <svante@swantzter.se>
2024-12-05 14:51:11 +0100

    I got hit with the unfortunate reality of python sort's stability

--- 2024/05/part2.py
+++ 2024/05/part2.py
@@ -19,21 +19,12 @@ def parse() -> tuple[list[tuple[int, int]], list[list[int]]]:
     return (rules, updates)
 
 
-def create_key_fn(rules: list[tuple[int, int]]):
-    def comparator(a: int, b: int) -> int:
-        rule = next((rule for rule in rules if (a, b) == rule), None)
-        if rule:
-            if a < rule[1]:
-                return 1
-            if a > rule[1]:
-                return -1
-        return 0
-
-    return cmp_to_key(comparator)
+def passes_rule(update: list[int], rule: tuple[int, int]) -> bool:
+    return update.index(rule[0]) < update.index(rule[1])
 
 
 def is_update_valid(update: list[int], rules: list[tuple[int, int]]) -> bool:
-    return all(map(lambda rule: update.index(rule[0]) < update.index(rule[1]), rules))
+    return all(passes_rule(update, rule) for rule in rules)
 
 
 def main():
@@ -49,7 +40,17 @@ def main():
             continue
 
         while not is_update_valid(update, match_rules):
-            update.sort(key=create_key_fn(match_rules))
+            rule_idx = 0
+            while rule_idx < len(match_rules):
+                rule = match_rules[rule_idx]
+                if passes_rule(update, rule):
+                    rule_idx += 1
+                else:
+                    while not passes_rule(update, rule):
+                        # Move the value that has to be last one step later in the rule
+                        idx = update.index(rule[1])
+                        update[idx], update[idx + 1] = update[idx + 1], update[idx]
+                    rule_idx = 0
         valid.append(update)
 
     print(sum(update[len(update) // 2] for update in valid))
