af0390304a96446d56022104c660ab65a8665830
Svante Bengtson <svante@swantzter.se>
2024-12-09 12:24:28 +0100

    add day 7

---
+++ 2024/07/part2.py
@@ -0,0 +1,38 @@
+import sys
+import pathlib
+import itertools
+
+DEFAULT_FILE = pathlib.Path(__file__).parent.resolve().joinpath("input.txt")
+
+OPS = ["+", "*"]
+
+
+def parse() -> list[tuple[int, list[int]]]:
+    return [
+        (int(line.split(":")[0]), list(map(int, line.split(":")[1].split())))
+        for line in open(sys.argv[1] if len(sys.argv) > 1 else DEFAULT_FILE, "r")
+    ]
+
+
+def main():
+    tests = parse()
+
+    passing: list[int] = []
+    for res, terms in tests:
+        reductions = [terms.pop(0)]
+        while len(terms) > 0:
+            term = terms.pop(0)
+            new_red = []
+            for red in reductions:
+                new_red.append(red * term)
+                new_red.append(red + term)
+                new_red.append(int(f"{red}{term}"))
+            reductions = new_red
+        if res in reductions:
+            passing.append(res)
+
+    print(sum(passing))
+
+
+if __name__ == "__main__":
+    main()
